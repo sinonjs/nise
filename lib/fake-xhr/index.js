sinonjs/nise

commit d6da025ad974fa9c7af37fca260a6050ed05f05c

feat: Allow responding with Blob or ArrayBuffer when responseType is `blob` (#164)
* feat: Allow responding with Blob or ArrayBuffer when responseType is `blob`

* test: Add `setResponseBody` tests for responseType errors

lib/fake-xhr/index.js
@@ -113,6 +113,20 @@ function verifyResponseBodyType(body, responseType) {
             );
             error.name = "InvalidBodyException";
         }
+    } else if (responseType === "blob") {
+        if (
+            !isString &&
+            !(body instanceof ArrayBuffer) &&
+            supportsBlob &&
+            !(body instanceof Blob)
+        ) {
+            error = new Error(
+                "Attempted to respond to fake XMLHttpRequest with " +
+                    body +
+                    ", which is not a string, ArrayBuffer, or Blob."
+            );
+            error.name = "InvalidBodyException";
+        }
     } else if (!isString) {
         error = new Error(
             `Attempted to respond to fake XMLHttpRequest with ${body}, which is not a string.`
@@ -353,6 +367,10 @@ function fakeXMLHttpRequestFor(globalScope) {
                 return null;
             }
         } else if (supportsBlob && responseType === "blob") {
+            if (body instanceof Blob) {
+                return body;
+            }
+
             var blobOptions = {};
             if (contentType) {
                 blobOptions.type = contentType;

lib/fake-xhr/index.test.js
@@ -992,6 +992,30 @@ describe("FakeXMLHttpRequest", function() {
             }, "InvalidBodyException");
         });
 
+        it("throws if body is not a string or arraybuffer and responseType='arraybuffer'", function() {
+            var xhr = new FakeXMLHttpRequest();
+            xhr.open("GET", "/");
+            xhr.responseType = "arraybuffer";
+            xhr.send();
+            xhr.setResponseHeaders({});
+
+            assert.exception(function() {
+                xhr.setResponseBody({});
+            }, "InvalidBodyException");
+        });
+
+        it("throws if body is not a string, arraybuffer, or blob and responseType='blob'", function() {
+            var xhr = new FakeXMLHttpRequest();
+            xhr.open("GET", "/");
+            xhr.responseType = "blob";
+            xhr.send();
+            xhr.setResponseHeaders({});
+
+            assert.exception(function() {
+                xhr.setResponseBody({});
+            }, "InvalidBodyException");
+        });
+
         if (supportsArrayBuffer) {
             describe("with ArrayBuffer support", function() {
                 it("invokes onreadystatechange for each chunk when responseType='arraybuffer'", function() {
@@ -1798,6 +1822,45 @@ describe("FakeXMLHttpRequest", function() {
                     );
 
                     assertBlobMatches(this.xhr.response, "\xFF", done);
+                });
+
+                it("returns blob with correct binary data when using an arraybuffer", function(done) {
+                    this.xhr.responseType = "blob";
+                    this.xhr.open("GET", "/");
+                    this.xhr.send();
+
+                    var buffer = new Uint8Array([160, 64, 0, 0, 32, 193])
+                        .buffer;
+
+                    this.xhr.respond(
+                        200,
+                        { "Content-Type": "application/octet-stream" },
+                        buffer
+                    );
+
+                    assertBlobMatches(
+                        this.xhr.response,
+                        new Blob([buffer]),
+                        done
+                    );
+                });
+
+                it("returns blob with correct binary data when using a blob", function(done) {
+                    this.xhr.responseType = "blob";
+                    this.xhr.open("GET", "/");
+                    this.xhr.send();
+
+                    var blob = new Blob([
+                        new Uint8Array([160, 64, 0, 0, 32, 193]).buffer
+                    ]);
+
+                    this.xhr.respond(
+                        200,
+                        { "Content-Type": "application/octet-stream" },
+                        blob
+                    );
+
+                    assertBlobMatches(this.xhr.response, blob, done);
                 });
 
                 it("does parse utf-8 content outside ASCII range properly", function(done) {

commit fd7ff4368fd466486b3f9f7e69089b6f660d6849

Fix lint error
This was introduced in d6da025ad974fa9c7af37fca260a6050ed05f05c

lib/fake-xhr/index.js
@@ -121,9 +121,7 @@ function verifyResponseBodyType(body, responseType) {
             !(body instanceof Blob)
         ) {
             error = new Error(
-                "Attempted to respond to fake XMLHttpRequest with " +
-                    body +
-                    ", which is not a string, ArrayBuffer, or Blob."
+                `Attempted to respond to fake XMLHttpRequest with ${body}, which is not a string, ArrayBuffer, or Blob.`
             );
             error.name = "InvalidBodyException";
         }